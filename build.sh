48f3dfd567f6e2c8570c775e152725b3dd82d037
earezki <arezki.elmehdi@gmail.com>
2020-03-14 22:47:14 +0100

    add stack deploy

--- build.sh
+++ build.sh
@@ -2,3 +2,4 @@ mvn clean install
 mvn package
 docker build . --file ./Dockerfile --tag localhost:5000/sick-tracker
 docker push localhost:5000/sick-tracker
+docker stack deploy --compose-file docker-compose.yml sick-tracker
